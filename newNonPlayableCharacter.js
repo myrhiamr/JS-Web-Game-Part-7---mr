00fd29932fd3ac5856df9d2d6b7bae6dadf8b23d
Myrhiam Rodriguez <myrhiamrodriguez@Myrhiams-MacBook-Air.local>
2024-02-03 19:39:26 -0800

    finished assigment mr

--- newNonPlayableCharacter.js
+++ newNonPlayableCharacter.js
@@ -1,59 +1,61 @@
 function newNonPlayableCharacter(x, y) {
-    let element = newImage('assets/red-character/static.gif')
+    const element = newImage('assets/red-character/static.gif');
     element.style.zIndex = 1;
-    
+
     let direction = null;
 
     function moveCharacter() {
-        if (direction === 'west') {
-            x -= 1
-        }
-        if (direction === 'north') {
-            y += 1
-        }
-        if (direction === 'east') {
-            x += 1
-        }
-        if (direction === 'south') {
-            y -= 1
-        }
-        element.style.left = x + 'px'
-        element.style.bottom = y + 'px'
+        if (direction === 'west') x -= 1;
+        if (direction === 'north') y += 1;
+        if (direction === 'east') x += 1;
+        if (direction === 'south') y -= 1;
+
+        element.style.left = x + 'px';
+        element.style.bottom = y + 'px';
     }
 
-    setInterval(moveCharacter, 1)
+    setInterval(moveCharacter, 1);
 
-    function walkEast() {
-        direction = 'east'
-        element.src = `./assets/red-character/east.gif`
+    async function walk(direction, time, gifPath) {
+        element.src = `./assets/red-character/${gifPath}.gif`;
+        await sleep(time);
+        stop();
     }
 
-    function walkNorth() {
-        direction = 'north'
-        element.src = `./assets/red-character/north.gif`
-    }
-
-    function walkWest() {
-        direction = 'west'
-        element.src = `./assets/red-character/west.gif`
-    }
-
-    function walkSouth() {
-        direction = 'south'
-        element.src = `./assets/red-character/south.gif`
-    }
+    const walkEast = async (time) => walk('east', time, 'east');
+    const walkNorth = async (time) => walk('north', time, 'north');
+    const walkWest = async (time) => walk('west', time, 'west');
+    const walkSouth = async (time) => walk('south', time, 'south');
 
     function stop() {
-        direction = null
-        element.src = `./assets/red-character/static.gif`
+        direction = null;
+        element.src = `./assets/red-character/static.gif`;
     }
 
-    return {
-        element: element,
-        walkWest: walkWest,
-        walkNorth: walkNorth,
-        walkEast: walkEast,
-        walkSouth: walkSouth,
-        stop: stop
+    async function moveNPC() {
+        await walkNorth(1400);
+        await walkEast(1200);
+        await walkSouth(300);
+        await walkEast(1500);
+        await walkSouth(1500);
+        await walkWest(2700);
+        await walkNorth(400);
     }
+
+    moveNPC();
+
+    return {
+        element,
+        walkWest,
+        walkNorth,
+        walkEast,
+        walkSouth,
+        stop
+    };
+}
+
+function sleep(time) {
+    return new Promise(resolve => {
+        setTimeout(resolve, time);
+    });
 }
